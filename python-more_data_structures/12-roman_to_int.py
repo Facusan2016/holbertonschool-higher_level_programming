861a3af0e093525e9f60c0c3734190d32113aff7
Facusan2016 <facusan2016@gmail.com>
2023-09-25 16:13:30 +0000

    feat exercises 0-12, docs: README.md

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    total = 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ls = len(roman_string)
+    for i in range(ls):
+        if i + 1 < ls and roman[roman_string[i]] < roman[roman_string[i + 1]]:
+            total -= roman[roman_string[i]]
+        else:
+            total += roman[roman_string[i]]
+    return total
