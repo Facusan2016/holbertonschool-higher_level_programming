03a050ae3b87dc2ca1cc61d463ce346d80e48b3b
Facusan2016 <facusan2016@gmail.com>
2023-09-26 07:02:09 +0000

    exercise 0 styles corrected

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     while i < x:
         try:
             print("{}".format(my_list[i]), end="")
-            i+=1
-            count+=1
+            i += 1
+            count += 1
         except Exception:
             break
     print()
