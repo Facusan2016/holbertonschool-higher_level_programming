45323052fa4188954b829504dfe957284e8ccdae
Facusan2016 <facusan2016@gmail.com>
2023-09-28 01:55:17 +0000

    docs corrected exercise 0 and 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+    File containing a Square class that have multiple
+    attributes and methods.
+"""
+
+
 class Square:
+
     """Class that describes a Square with properties."""
+
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+
+"""
+    File containing a Square class that have multiple
+    attributes and methods.
+"""
+
+
 class Square:
     """Class that describes a Square with properties."""
+
     def __init__(self, size):
-        """Init method that initializes each object (instance) of the class"""
-        self.__size = size;
+        """
+            Init method that initializes each object (instance) of the class
+            with __size being a private attribute.
+        """
+        self.__size = size
