f05b5881af36713efd9d18afc8fb3ee15318ec58
Facusan2016 <facusan2016@gmail.com>
2023-09-29 11:26:35 +0000

    fix exericse 6 printing

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -73,9 +73,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            if self.__position[1] > 0:
-                for _ in range(self.__position[1]):
-                    print()
+            for _ in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 print(" "*self.__position[0], end="")
                 for j in range(self.__size):
