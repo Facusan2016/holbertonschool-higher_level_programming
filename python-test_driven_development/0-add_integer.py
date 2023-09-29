4e83550fdc141bd8fb0f179f57ee1f12fc8823a3
Facusan2016 <facusan2016@gmail.com>
2023-09-29 15:14:41 +0000

    fix exercise 0 and tests added

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,16 +10,16 @@ def add_integer(a, b=98):
     """
     Adds a and b just if they're int or floats,
     """
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+
     if isinstance(a, float):
         a = int(a)
 
     if isinstance(b, float):
         b = int(b)
-
-    if not isinstance(a, int):
-        raise TypeError('a must be an integer')
-
-    if not isinstance(b, int):
-        raise TypeError('b must be an integer')
 
     return a + b
