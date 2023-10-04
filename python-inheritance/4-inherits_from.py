e48bc6250a13c386e617ded77ec1f147d5b93f81
Facusan2016 <facusan2016@gmail.com>
2023-10-04 15:50:26 +0000

    fix exercise 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,7 +11,7 @@ def inherits_from(obj, a_class):
         if the object is an instance of a class that inherited
         (directly or indirectly) from the specified class
     """
-    if issubclass(type(obj), a_class) and not isinstance(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
