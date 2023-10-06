340ae0fe11bacbba6509f46223ee247a2642bae5
Facusan2016 <facusan2016@gmail.com>
2023-10-06 15:36:15 +0000

    added exercise 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    Module that define the Student class
+"""
+
+
+class Student:
+
+    """
+        Student class that has first_name
+        last_name and age attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
