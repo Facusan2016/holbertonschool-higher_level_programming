fa4bd823ee0bc310e38dc76082eb514b9bdc2bca
Facusan2016 <facusan2016@gmail.com>
2023-11-14 16:15:03 -0300

    docs README.md, exercise 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2.
+
+-- The script itself
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
