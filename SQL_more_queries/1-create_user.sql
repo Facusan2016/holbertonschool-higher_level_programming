a7f038c8bfaad2614e320a14e311492fe14e0aeb
Facusan2016 <facusan2016@gmail.com>
2023-11-14 16:26:12 -0300

    exercise 1 added

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- The script itself
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
