e313c12aba090309fa178492036aa634c2e47818
Facusan2016 <facusan2016@gmail.com>
2023-11-14 19:59:57 -0300

    added exercises 7-10

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all cities contained in the database hbtn_0d_usa.
+
+-- The script itself
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id;
