d444c59c6fce60e0803a281dafbc777785879305
Facusan2016 <facusan2016@gmail.com>
2023-11-16 08:29:27 -0300

    added exercise 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+  All states via SQLAlchemy.
+
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+      Main function.
+    """
+    conn = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(conn.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+
+    for pos, row in enumerate(states, 1):
+        print(f"{pos}:{row.name}")
